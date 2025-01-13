57acf135520f9e8f5e092f3c835cc1095c68d1ae
ajmalrashadmarkz <ajmalrashadmarkz@gmail.com>
2025-01-13 15:47:20 +0530

    survey_kerala

--- admin_dashboard/admin.py
+++ admin_dashboard/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Survey, Question, Choice, Submission
 
 # Register your models here.
+class QuestionInline(admin.TabularInline):
+  model = Question
+  show_change_link = True
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+
+
+class SurveyAdmin(admin.ModelAdmin):
+  inlines = [
+    QuestionInline
+  ]
+
+class QuestionAdmin(admin.ModelAdmin):
+  inlines = [
+    ChoiceInline
+  ]
+  
+class SubmissionAdmin(admin.ModelAdmin):
+  list_display = ('participant_email', 'status')
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission, SubmissionAdmin)
